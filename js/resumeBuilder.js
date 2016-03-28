f528bc63e3e856abeed4ef8c5d2ea593db54abac
Shakil Sultan Ali <shakil.sultan@gmail.com>
2016-03-28 15:48:28 +0500

    complete lesson 2

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,43 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var bio = {
+  "name" : "Shakil",
+  "role" : "Front-End Web Developer",
+  "contact" : {
+  	"email" : "shakil.sultan@gmail.com",
+  	"github" : "https://github.com/shakilsultan",
+  	"twitter" : "https://twitter.com/shakilsul",
+  	"location" : "Karachi, Pakistan"
+ 	},
+  	"headshot" : "images/shakil.jpg",
+  	"welcomeMessage" : "Welcome to my Resume Page!",
+  	"skills" : [ "SEO", "HTML", "CSS", "JavaScript" ]
+};
+
+var work = {
+	"jobs" : [ {
+    	"jobPosition" : "SEO Lead / Front-End Web Developer",
+		"employer" : "Be Good Real Estate",
+		"yearsWorked" : 0.2
+	},
+	{
+		"jobPosition" : "Senior Webmaster",
+		"employer" : "iVortexMedia",
+		"yearsWorked" : 7
+	}
+	]
+};
+
+
+var education = {
+	"schools" : [ {
+		"name" : "APTECH",
+		"city" : "Karachi",
+		"majors" : "Software Engineering Diploma"
+	},
+	{
+		"name" : "NewPorts",
+		"city" : "Karachi",
+		"majors" : "BS - Computer Science"
+	}
+	]
+};
+
